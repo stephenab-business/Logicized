97881ae0d7c200e37f216700ad1b1882c13c406a
Stephen Brown <stephen@Stephens-MBP.lan>
2022-02-13 19:27:37 -0500

    Resolved, hopefully, the last time

--- src/CanvasEditor/PartsMenu.tsx
+++ src/CanvasEditor/PartsMenu.tsx
@@ -1,44 +1,20 @@
-<<<<<<< HEAD
 import { Elements, isNode, removeElements, } from 'inputs-and-outputs-renderer';
-=======
-<<<<<<< HEAD
-import { Elements, isNode, removeElements, } from 'inputs-and-outputs-renderer';
-=======
->>>>>>> 07a57989e6bb039742a68357a009bb1a87d4b15d
->>>>>>> 048ec59465fccf702f60c5a32f884538b15490ca
 import React, { DragEvent, FC } from 'react';
 
 interface PartsMenuProps {
   editing: boolean;
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 048ec59465fccf702f60c5a32f884538b15490ca
   setEditing: React.Dispatch<React.SetStateAction<boolean>>;
   elements: Elements;
   setElements: React.Dispatch<React.SetStateAction<Elements>>;
 }
 
 export const PartsMenu: FC<PartsMenuProps> = ({editing, setEditing, elements, setElements}) => {
-<<<<<<< HEAD
-=======
-=======
-  setEditing: React.Dispatch<React.SetStateAction<boolean>>; 
-}
-
-export const PartsMenu: FC<PartsMenuProps> = ({editing, setEditing}) => {
->>>>>>> 07a57989e6bb039742a68357a009bb1a87d4b15d
->>>>>>> 048ec59465fccf702f60c5a32f884538b15490ca
   const onDragStart = (event: DragEvent, nodeType: string) => {
     event.dataTransfer.setData('application/reactflow', nodeType); 
     event.dataTransfer.effectAllowed = 'move';
   };
 
   const changeMode = () => {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 048ec59465fccf702f60c5a32f884538b15490ca
     elements.forEach((element) => {
       if (isNode(element) && element.data.modeIsEditing !== undefined) {
         const newElements = elements;
@@ -57,28 +33,12 @@ export const PartsMenu: FC<PartsMenuProps> = ({editing, setEditing}) => {
 
   
 
-<<<<<<< HEAD
-=======
-=======
-    setEditing(!editing);
-  }
-
->>>>>>> 07a57989e6bb039742a68357a009bb1a87d4b15d
->>>>>>> 048ec59465fccf702f60c5a32f884538b15490ca
   return (
     <aside>
       {editing && 
       <div>
         <h3>Parts</h3>
-<<<<<<< HEAD
         {/* <div className="react-flow__node-input" onDragStart = {(event: DragEvent) => onDragStart(event, 'input')} draggable>
-=======
-<<<<<<< HEAD
-        {/* <div className="react-flow__node-input" onDragStart = {(event: DragEvent) => onDragStart(event, 'input')} draggable>
-=======
-        <div className="react-flow__node-input" onDragStart = {(event: DragEvent) => onDragStart(event, 'input')} draggable>
->>>>>>> 07a57989e6bb039742a68357a009bb1a87d4b15d
->>>>>>> 048ec59465fccf702f60c5a32f884538b15490ca
           Input Node
         </div>
         <div className="react-flow__node-output" onDragStart = {(event: DragEvent) => onDragStart(event, 'output')} draggable>
@@ -86,15 +46,7 @@ export const PartsMenu: FC<PartsMenuProps> = ({editing, setEditing}) => {
         </div>
         <div className = "dndnode-horizontal-input" onDragStart = {(event: DragEvent) => onDragStart(event, 'horizontal-input')} draggable>
           Horizontal Input Node
-<<<<<<< HEAD
         </div> */}
-=======
-<<<<<<< HEAD
-        </div> */}
-=======
-        </div>
->>>>>>> 07a57989e6bb039742a68357a009bb1a87d4b15d
->>>>>>> 048ec59465fccf702f60c5a32f884538b15490ca
         <div className = 'dndnode-and' onDragStart = {(event: DragEvent) => onDragStart(event, 'and')} draggable>
           AND
         </div>
@@ -125,18 +77,9 @@ export const PartsMenu: FC<PartsMenuProps> = ({editing, setEditing}) => {
         <div className="dndnode-xnor" onDragStart = {(event: DragEvent) => onDragStart(event, 'xnor')} draggable>
           XNOR
         </div>
-<<<<<<< HEAD
         <div className="dndnode-clock" onDragStart = {(event: DragEvent) => onDragStart(event, 'clock')} draggable>
           Clock
         </div>
-=======
-<<<<<<< HEAD
-        <div className="dndnode-clock" onDragStart = {(event: DragEvent) => onDragStart(event, 'clock')} draggable>
-          Clock
-        </div>
-=======
->>>>>>> 07a57989e6bb039742a68357a009bb1a87d4b15d
->>>>>>> 048ec59465fccf702f60c5a32f884538b15490ca
       </div>
       }
       {!editing &&
@@ -145,14 +88,7 @@ export const PartsMenu: FC<PartsMenuProps> = ({editing, setEditing}) => {
           <div className="dndnode-clock" onDragStart = {(event: DragEvent) => onDragStart(event, 'clock')} draggable>
             Clock
           </div>
-<<<<<<< HEAD
           <button></button>
-=======
-<<<<<<< HEAD
-          <button></button>
-=======
->>>>>>> 07a57989e6bb039742a68357a009bb1a87d4b15d
->>>>>>> 048ec59465fccf702f60c5a32f884538b15490ca
         </div>
       }
       <button onClick={changeMode}>Change Mode</button>
