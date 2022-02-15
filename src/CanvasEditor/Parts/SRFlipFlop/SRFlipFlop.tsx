45d85ca02cc1c82521cc620950c0e59372fe8bce
Amber <77752815+amberspease@users.noreply.github.com>
2022-02-14 20:27:46 -0500

    Progress in Complex Nodes

---
+++ src/CanvasEditor/Parts/SRFlipFlop/SRFlipFlop.tsx
@@ -0,0 +1,5 @@
+import React, { memo, FC, useEffect } from 'react';
+
+import { Handle, Position, NodeProps, Connection, Edge, useStoreState, ReactFlowState, Node, } from 'inputs-and-outputs-renderer';
+import { getInputPosition, getOutputPosition } from '../../Functions/gateFunctions'
+import './SRFlipFlopNode.css';
